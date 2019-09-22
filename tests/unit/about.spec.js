99bfe222e5fa8fd96ec25f58b528ef47a7222a15
Juliano Barboza <julianodawid@gmail.com>
2019-09-21 21:35:02 -0300

    fixes test pre-hook

--- tests/unit/about.spec.js
+++ tests/unit/about.spec.js
@@ -4,6 +4,6 @@ import About from "@/components/About.vue";
 describe("About.vue", () => {
   it("has a title", () => {
     const wrapper = shallowMount(About);
-    expect(wrapper.text()).toHave("title");
+    expect(wrapper.text()).toHave("title"); 
   });
 });
