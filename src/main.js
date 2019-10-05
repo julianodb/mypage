38134d0182375e7964b4d2f014140b3bad4dc816
Juliano Barboza <julianodawid@gmail.com>
2019-10-05 20:55:43 -0300

    adds firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import * as firebase from "firebase/app";
+import firebaseConfig from "./firebase.config.js";
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
